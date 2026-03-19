c0461c89eae5087e884cc3889ba541ae6f78d0ae
0xardy <ardy@wonderland.xyz>
2026-03-19 14:07:59 -0300

    chore: update multicall address

--- src/migration/utils/constants.ts
+++ src/migration/utils/constants.ts
@@ -16,9 +16,9 @@ export const MIGRATION_MESSAGES = {
  * amount is 0, causing a FailedToSendNativeAsset() revert. This Multicall3 variant
  * includes a payable fallback so it can safely act as the withdrawal processor.
  *
- * mainnet: https://etherscan.io/address/0xe3b3DBB09193Db1BC4A37f1Fc2ac027A11b76134#code
- * optimism: https://optimistic.etherscan.io/address/0xe3b3DBB09193Db1BC4A37f1Fc2ac027A11b76134#code
- * arbitrum: https://arbiscan.io/address/0xe3b3DBB09193Db1BC4A37f1Fc2ac027A11b76134#code
- * bsc: https://bscscan.com/address/0xe3b3DBB09193Db1BC4A37f1Fc2ac027A11b76134#code
+ * mainnet: https://etherscan.io/address/0x35C06897600d29587515C526e523984e176Bf747#code
+ * optimism: https://optimistic.etherscan.io/address/0x35C06897600d29587515C526e523984e176Bf747#code
+ * arbitrum: https://arbiscan.io/address/0x35C06897600d29587515C526e523984e176Bf747#code
+ * bsc: https://bscscan.com/address/0x35C06897600d29587515C526e523984e176Bf747#code
  */
-export const MULTICALL3_WITH_FALLBACK = '0xe3b3DBB09193Db1BC4A37f1Fc2ac027A11b76134';
+export const MULTICALL3_WITH_FALLBACK = '0x35C06897600d29587515C526e523984e176Bf747';
